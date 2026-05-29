3bc528bf2fc65f9771edce8a53d1bafda8240a95
ash <lixkram@gmail.com>
2026-05-29 13:28:31 +0300

    - added tiled - added loading of level_1

---
+++ assets/tiled/enviroment.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="enviroment" tilewidth="8" tileheight="8" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../pictures/level1/block1.png" width="8" height="8"/>
+ </tile>
+</tileset>
